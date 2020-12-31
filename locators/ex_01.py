2a2efc94dc26eb823e576368a8064650929fa114
v ubuntu <rafalsobieszczyk@gmail.com>
2020-12-31 04:07:41 +0100

    ex01 end

--- locators/ex_01.py
+++ locators/ex_01.py
@@ -8,6 +8,7 @@ ex_01 = {
     "button2": (By.XPATH, "//button[@id='btnButton2']"),
     "solution": (By.XPATH, "//button[@id='solution']"),
     "trial": (By.XPATH, "//pre[@id='trail']"),
+    "trial_set_to": (By.XPATH, "//*[contains(text(),'Trail set to')]"),
     "good_answer": (By.XPATH, "//code[contains(text(),'OK. Good answer')]"),
     "h1_exercise": (By.XPATH, "//h1[contains(text(),'Exercise 1 - Three buttons')]"),
 }
